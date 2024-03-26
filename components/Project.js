1f562286229016beb60338146ea7fcad54c758e4
Charlineoe <ch-oe@gmx.de>
2024-03-26 12:02:24 +0100

    Change h3 to h2

--- components/Project.js
+++ components/Project.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 export default function Project({ title, duration, complexity }) {
   return (
     <StyledArticle>
-      <h3>{title}</h3>
+      <h2>{title}</h2>
       <p>Duration: {duration}</p>
       <p>Complexity: {complexity}</p>
     </StyledArticle>
@@ -11,7 +11,6 @@ export default function Project({ title, duration, complexity }) {
 
 const StyledArticle = styled.article`
   border: 1px solid black;
-  margin: 10px;
   padding: 0px 0px 0px 10px;
   width: 90vw;
 `;
