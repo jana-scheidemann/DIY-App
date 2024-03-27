35ad222f46c5c7fefc37db9d345ed01f7f2c60b8
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-03-27 09:47:09 +0100

    add button: back to all

--- pages/[id].js
+++ pages/[id].js
@@ -1,9 +1,24 @@
-import { useRouter } from "next/router";
 import ProjectDetails from "@/components/ProjectDetails";
+import { initialProjects } from "@/db/data";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function ProjectDetailPage() {
   const router = useRouter();
   const { id } = router.query;
 
-  return <ProjectDetails />;
+  const currentProject = initialProjects.find(
+    (initialProject) => initialProject.id.toString() === id
+  );
+
+  if (!currentProject) {
+    return null;
+  }
+
+  return (
+    <>
+      <ProjectDetails currentProject={currentProject} />
+      <Link href={"/"}>← back to all projects</Link>
+    </>
+  );
 }
