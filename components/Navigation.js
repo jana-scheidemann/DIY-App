d8d7a8484a3a8bd11a922328eef3718e27e2e15a
Charlineoe <ch-oe@gmx.de>
2024-03-28 09:15:47 +0100

    Create component Navigation, create Link to Home and New Project

---
+++ components/Navigation.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/create">New Project</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
