3c6d9f4e0fb6c8d9a216dc740b6c8739fb9045af
Charlineoe <ch-oe@gmx.de>
2024-03-28 09:11:58 +0100

    Add useState, add handle function for ProjectFormNew

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import GlobalStyle from "../styles";
+import { useState } from "react";
+import { initialProjects } from "@/db/data";
+import { uid } from "uid";
 
 export default function App({ Component, pageProps }) {
+  const [projects, setProjects] = useState(initialProjects);
+
+  function handleAddProject(newProject) {
+    setProjects([{ id: uid(), ...newProject }, ...projects]);
+  }
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        projects={projects}
+        onAddProject={handleAddProject}
+      />
     </>
   );
 }
