d0660844f9ec531f8c77af95c801a1c7a87fac3e
Charlineoe <ch-oe@gmx.de>
2024-04-11 11:35:24 +0200

    add and change styling in overview

--- components/Project.js
+++ components/Project.js
@@ -13,11 +13,11 @@ export default function Project({
 }) {
   return (
     <StyledArticle>
-      <Link href={`/projects/${slug}`}>
-        <h2>{title}</h2>
-      </Link>
-      <p>Duration: {duration}</p>
-      <p>Complexity: {complexity}</p>
+      <StyledLink href={`/projects/${slug}`}>
+        <StyledHeadline>{title}</StyledHeadline>
+      </StyledLink>
+      <StyledParagraph>Duration: {duration}</StyledParagraph>
+      <StyledParagraph>Complexity: {complexity}</StyledParagraph>
       <FavoriteButton
         id={id}
         onToggleFavorite={onToggleFavorite}
@@ -28,7 +28,40 @@ export default function Project({
 }
 
 const StyledArticle = styled.article`
-  border: 1px solid black;
-  padding: 0px 0px 0px 10px;
+  background-color: white;
+  //border: 1px solid black;
+  border-radius: 20px;
   width: 90vw;
+  transform: scale(0.9);
+  transition: 0.3s;
+  &:hover {
+    transform: scale(1);
+    transition: 0.5s;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  background-color: var(--background-color-blue);
+  color: var(--text-color);
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  text-decoration: none;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const StyledHeadline = styled.h2`
+  //text-align: center;
+  font-size: 20px;
+  color: #282828;
+  margin: 7px;
+  text-decoration: none;
+`;
+const StyledParagraph = styled.p`
+  text-align: left;
+  font-size: 15px;
+  color: #282828;
+  margin: 7px;
+  text-decoration: none;
 `;
