ebb2507b615f7696f1fdfc8185ff4acf68c44241
Charlineoe <ch-oe@gmx.de>
2024-04-11 11:51:12 +0200

    change globalStyles

--- styles.js
+++ styles.js
@@ -1,6 +1,24 @@
 import { createGlobalStyle } from "styled-components";
-
 export default createGlobalStyle`
+
+:root{
+    --background-color: #F2F2F2; //F2F2F2 = Off-white color
+    --font-size: 16px;
+    --text-color: #282828; 
+    //--background-color-accent: #FF5733;
+    --background-color-yellow: #DBF22E;
+    --background-color-blue: #2ABFB0;
+    
+    //--secondary-color: #2abfb0;
+    --border: 1px solid black;
+    --border-radius: 70px;
+  }
+  //F2F2F2 = Off-white color --Background
+  //#282828 = Blackish color -- Text
+  //#FF5733 = Orange color --Highlight
+  //#dbf22e = Yellow color
+  //#2abfb0 = türkis color
+
   *,
   *::before,
   *::after {
@@ -10,5 +28,13 @@ export default createGlobalStyle`
   body {
     margin: 0;
     font-family: system-ui;
+
   }
+h4{
+  margin-block-end: 0;
+  font-size: 16px;
+  
+
+}
+
 `;
