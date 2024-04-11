4a2097622b77860b21a84ae9378206f8f4dac438
Charlineoe <ch-oe@gmx.de>
2024-04-11 11:50:25 +0200

    remove link back home, moved to projectDetails

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -1,6 +1,7 @@
 import ProjectDetails from "@/components/ProjectDetails";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import styled from "styled-components";
 
 export default function ProjectDetailPage({
   projects,
@@ -25,7 +26,19 @@ export default function ProjectDetailPage({
         onToggleFavorite={onToggleFavorite}
         onEditProject={onEditProject}
       />
-      <Link href={"/"}>← back to all projects</Link>
     </>
   );
 }
+const StyledLink = styled(Link)`
+  background-color: var(--background-color-blue);
+  color: var(--text-color);
+  border-radius: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  text-decoration: none;
+  padding: 5px;
+  margin: 5px 0;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
